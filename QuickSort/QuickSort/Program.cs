81213c9a8510bcfd852068888cc9c4cfc1fee68c
Matveev_AS <kolonka.power@mail.ru>
2025-03-25 13:35:28 +0400

    Добавьте файлы проекта.

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace QuickSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Quiksort(new int[] { 9, 5, 2, 8, 3, 7, 4, 6, 1 });
+            foreach (int i in arr)
+                Console.WriteLine(i);
+            Console.ReadKey();
+        }
+
+        static int[] Quiksort(int[] arr)
+        {
+            if (arr.Length < 2)
+            {
+                return arr;
+            }
+            else
+            {
+                var pivot = arr[0];
+                var less = arr.Skip(1).Where(i => i < pivot);
+                var greater = arr.Skip(1).Where(i => i > pivot);
+
+                return (Quiksort(less.ToArray()).Concat(new int[] { arr[0] }).Concat(Quiksort(greater.ToArray()).ToArray())).ToArray();
+            }
+        }
+    }
+}
---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Buffers;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 2, 2, 5, 6, 7, 6 };
+            var temp = new int[arr.Length - 1];
+            Array.Copy(arr, 1, temp, 0, temp.Length);
+            var sum = arr[0] + Sum(temp);
+            Console.WriteLine(sum);
+            Console.WriteLine(FindMax(new List<int> { 1, 2, 2, 2, 5, 6, 7, 6 }));
+            Console.ReadKey();
+        }
+
+        static int Sum(int[] arr)
+        {
+            if (arr.Length == 1)
+            {  
+                return arr[0]; 
+            }
+            else
+            {
+                var temp = new int[arr.Length - 1];
+                Array.Copy(arr, 1, temp, 0, temp.Length);
+                return arr[0] + Sum(temp);
+            }
+        }
+
+        static int FindMax(List<int> list)
+        {
+            if (list.Count == 1)
+            {  
+                return list[0];
+            }
+            else
+            {
+                return list.FirstOrDefault() > FindMax(list.Skip(1).ToList()) ? list.FirstOrDefault() : FindMax(list.Skip(1).ToList());
+            }
+        }
+    }
+}
